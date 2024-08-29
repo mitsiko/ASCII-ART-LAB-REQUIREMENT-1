bd8ddd773bf196bf86d3110c9a4bdd916a35bf8e
Lee Michiko Garcia <michikogarciasp@gmail.com>
2024-08-30 02:41:37 +0800

    Update GARCIA-Lee-Michiko-ASCII-ART.py

--- GARCIA-Lee-Michiko-ASCII-ART.py
+++ GARCIA-Lee-Michiko-ASCII-ART.py
@@ -3,19 +3,24 @@ katakana = "\33[35m"
 name1 = "\33[33m"
 name2 = "\33[32m"
 
-print("")
-print(f" {squares}.------..------..------. {katakana}         #     #                         # ")
-print(f" {squares}| .--. || .--. || .--. | {katakana}       #  ##########   ##          #   #   ")
-print(f" {squares}| :/\: || (\/) || (\/) | {katakana}      ##       #       #  ##         # #   ")
-print(f" {squares}| (__) || :\/: || :\/: | {katakana}   ## #       #      #     ##        #     ")
-print(f" {squares}| '--' || '--' || '--' | {katakana}  ##   #      #              ##    ## #    ")
-print(f" {squares}`------'`------'`------' {katakana}  #      #      ####              ##    #  ")
-print(f" {name2}                                                       __   ___  ___            ")
-print(f" {name1}███╗   ███╗██╗ ██████╗██╗  ██╗██╗██╗  ██╗ ██████╗  {name2}   / /  / _/ / _/     ")
-print(f" {name1}████╗ ████║██║██╔════╝██║  ██║██║██║ ██╔╝██╔═══██╗ {name2}  / /_ / _/ / _/      ")
-print(f" {name1}██╔████╔██║██║██║     ███████║██║█████╔╝ ██║   ██║ {name2} /___//___//___/      ")
-print(f" {name1}██║╚██╔╝██║██║██║     ██╔══██║██║██╔═██╗ ██║   ██║                              ")
-print(f" {name1}██║ ╚═╝ ██║██║╚██████╗██║  ██║██║██║  ██╗╚██████╔╝ {squares} ███╗  ███╗  ███╗   ")
-print(f" {name1}╚═╝     ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝ ╚═════╝  {squares}  ╚═╝   ╚═╝   ╚═╝   ") 
+print("") # line space for aesthetic purposes
+print("") # line space for aesthetic purposes
+
+print(f"{squares}.------..------..------. {katakana}         #     #                         # ")
+print(f"{squares}| .--. || .--. || .--. | {katakana}       #  ##########   ##          #   #   ")
+print(f"{squares}| :/\: || (\/) || (\/) | {katakana}      ##       #       #  ##         # #   ")
+print(f"{squares}| (__) || :\/: || :\/: | {katakana}   ## #       #      #     ##        #     ")
+print(f"{squares}| '--' || '--' || '--' | {katakana}  ##   #      #              ##    ## #    ")
+print(f"{squares}`------'`------'`------' {katakana}  #      #      ####              ##    #  ")
+print(f"{name2}                                                       __   ___  ___            ")
+print(f"{name1}███╗   ███╗██╗ ██████╗██╗  ██╗██╗██╗  ██╗ ██████╗  {name2}   / /  / _/ / _/     ")
+print(f"{name1}████╗ ████║██║██╔════╝██║  ██║██║██║ ██╔╝██╔═══██╗ {name2}  / /_ / _/ / _/      ")
+print(f"{name1}██╔████╔██║██║██║     ███████║██║█████╔╝ ██║   ██║ {name2} /___//___//___/      ")
+print(f"{name1}██║╚██╔╝██║██║██║     ██╔══██║██║██╔═██╗ ██║   ██║                              ")
+print(f"{name1}██║ ╚═╝ ██║██║╚██████╗██║  ██║██║██║  ██╗╚██████╔╝ {squares} ███╗  ███╗  ███╗   ")
+print(f"{name1}╚═╝     ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝ ╚═════╝  {squares}  ╚═╝   ╚═╝   ╚═╝   ") 
+
+print("") # line space for aesthetic purposes
+print("") # line space for aesthetic purposes
 
  
